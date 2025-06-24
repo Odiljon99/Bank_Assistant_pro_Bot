d75444e8e9336bf1eecc9219db73b30679945651
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-24 16:05:16 +0500

    Delete app/app directory

--- app/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
-WEBHOOK_PATH = f"/webhook/{WEBHOOK_SECRET}"
