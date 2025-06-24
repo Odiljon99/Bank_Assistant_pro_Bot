c461912989803aee181350d19c0b12f969a6c3cb
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-24 16:07:28 +0500

    Create messages.py

---
+++ app/messages.py
@@ -0,0 +1,16 @@
+langs = {
+    "ru": {
+        "menu": "📋 Главное меню:",
+        "admin_panel": "🔧 Панель администратора",
+        "new_report": "🆕 Новая заявка:",
+        "back": "🔙 Назад",
+        "start": "Привет! Выберите язык."
+    },
+    "uz": {
+        "menu": "📋 Asosiy menyu:",
+        "admin_panel": "🔧 Admin panel",
+        "new_report": "🆕 Yangi muammo:",
+        "back": "🔙 Orqaga",
+        "start": "Salom! Tilni tanlang."
+    }
+}
