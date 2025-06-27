b90e156c6443505a48ba1792603e37251212ddfb
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-27 08:51:44 +0500

    Update server.py

--- server.py
+++ server.py
@@ -6,14 +6,17 @@ from app import bot, dp
 from app import config
 from app.database import cursor, conn
 from app.handlers import router
+import os
 
 dp.include_router(router)
 
 async def on_startup():
-    await bot.set_webhook(f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}{config.WEBHOOK_PATH}")
+    await bot.set_webhook(f"https://{os.getenv('RAILWAY_STATIC_URL')}{config.WEBHOOK_PATH}")
 
 app = web.Application()
 setup_application(app, dp, bot=bot)
 app.router.add_route("*", config.WEBHOOK_PATH, SimpleRequestHandler(dispatcher=dp, bot=bot))
+
 if __name__ == "__main__":
+    asyncio.run(on_startup())
     web.run_app(app, host="0.0.0.0", port=10000)
