c649e4237bf7fcfbb2c1719836c853757be4a363
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-27 21:22:08 +0500

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -9,3 +9,19 @@ def get_main_menu(lang):
             [KeyboardButton(langs[lang]["admin_panel"])],
         ]
     )
+
+def get_language_keyboard():
+    return ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[
+            [KeyboardButton("🇷🇺 Русский"), KeyboardButton("🇺🇿 O‘zbek")],
+        ]
+    )
+
+def get_agree_keyboard(lang):
+    return ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[
+            [KeyboardButton("✅ Я согласен" if lang == "ru" else "✅ Men roziman")],
+        ]
+    )
