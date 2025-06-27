ab0378daded70f4f79555b984aa1607abb906e8d
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-27 20:29:43 +0500

    Update messages.py

--- app/messages.py
+++ app/messages.py
@@ -4,13 +4,35 @@ langs = {
         "admin_panel": "🔧 Панель администратора",
         "new_report": "🆕 Новая заявка:",
         "back": "🔙 Назад",
-        "start": "Привет! Выберите язык."
+        "start": "Привет! Выберите язык.",
+        "warning_text": (
+            "⚠️ ВНИМАНИЕ!\n\n"
+            "Отправляя свои данные, вы соглашаетесь на их обработку.\n"
+            "Мы гарантируем конфиденциальность и безопасность.\n\n"
+            "Если вы согласны, нажмите кнопку ниже."
+        ),
+        "start_registration": "📝 Начнем регистрацию. Пожалуйста, ответьте на следующие вопросы.",
+        "ask_full_name": "📛 Введите ваше полное имя:",
+        "ask_phone": "📞 Введите ваш номер телефона:",
+        "ask_birthday": "📅 Введите вашу дату рождения (дд.мм.гггг):",
+        "ask_pinfl": "🆔 Введите ваш ПИНФЛ:"
     },
     "uz": {
         "menu": "📋 Asosiy menyu:",
         "admin_panel": "🔧 Admin panel",
         "new_report": "🆕 Yangi muammo:",
         "back": "🔙 Orqaga",
-        "start": "Salom! Tilni tanlang."
+        "start": "Salom! Tilni tanlang.",
+        "warning_text": (
+            "⚠️ DIQQAT!\n\n"
+            "Maʼlumotlaringizni yuborish orqali siz ularni qayta ishlashga rozilik bildirasiz.\n"
+            "Biz maxfiylik va xavfsizlikni kafolatlaymiz.\n\n"
+            "Agar rozisiz, quyidagi tugmani bosing."
+        ),
+        "start_registration": "📝 Ro‘yxatdan o‘tishni boshlaymiz. Iltimos, quyidagi savollarga javob bering.",
+        "ask_full_name": "📛 To‘liq ismingizni kiriting:",
+        "ask_phone": "📞 Telefon raqamingizni kiriting:",
+        "ask_birthday": "📅 Tug‘ilgan kuningizni kiriting (kk.oo.yyyy):",
+        "ask_pinfl": "🆔 PINFL raqamingizni kiriting:"
     }
 }
