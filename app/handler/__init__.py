3855cc1c4980a494af0a1005606c412549bd6202
Odiljon99 <odiljonobidjonov8@gmail.com>
2025-06-30 10:04:33 +0500

    Update __init__.py

--- app/handler/__init__.py
+++ app/handler/__init__.py
@@ -1,7 +1,1 @@
-from aiogram import Router
-from app.main_handlers import router as main_router
-from app.handler.credit import router as credit_router
 
-router = Router()
-router.include_router(main_router)
-router.include_router(credit_router)
